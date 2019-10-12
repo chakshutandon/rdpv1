1d59c2642c83cb89e8eaf585a5ebe4f79263ff03
Chakshu Tandon <ct540@ilab1.cs.rutgers.edu>
2019-10-12 17:56:52 -0400

    Define constants

---
+++ const.py
@@ -0,0 +1,37 @@
+RDP_VERSION = 0x1
+
+# UDP supports max packet size of 64K bytes
+RDP_MAX_PACKET_LENGTH = 64 * 1024
+
+# For best match with hardware and network realities, the value of bufsize should be a relatively small power of 2, for example, 4096.
+UDP_BUFFER_SIZE = 4096
+
+# Flag Name       Byte Value (Hex)      Byte Value (Binary)     Meaning
+# SOCK352_SYN     0x01                  00000001                Connection initiation
+# SOCK352_FIN     0x02                  00000010                Connection end
+# SOCK352_ACK     0x04                  00000100                Acknowledgement number
+# SOCK352_RESET   0x08                  00001000                Reset connection
+# SOCK352_HAS_OPT 0xA0                  00010000                Option field is valid
+
+SOCK352_SYN      = 0b00000001
+SOCK352_FIN      = 0b00000010
+SOCK352_ACK      = 0b00000100
+SOCK352_RESET    = 0b00001000
+SOCK352_HAS_OPT  = 0b00010000
+
+# struct __attribute__ ((__packed__)) packet_header { 
+    # uint8_t version;              /* version number */
+    # uint8_t flags;                /* for connection set up, tear-down, control */
+    # uint8_t opt_ptr;              /* option type between the header and payload */
+    # uint8_t protocol;             /* higher-level protocol */
+    # uint16_t header_len;          /* length of the header */
+    # uint16_t checksum;            /* checksum of the packet */
+    # uint32_t source_port;         /* source port */
+    # uint32_t dest_port;           /* destination port */
+    # uint64_t sequence_no;         /* sequence number */
+    # uint64_t ack_no;              /* acknowledgement number */
+    # uint32_t window;              /* receiver advertised window in bytes*/
+    # uint32_t payload_len;         /* length of the payload */
+# };
+
+PACKET_HEADER_STRUCT = '!BBBBHHLLQQLL'
