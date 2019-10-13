cc26c1421803b224426441d7627ba8437d15fbb4
Chakshu Tandon <ct540@ilab3.cs.rutgers.edu>
2019-10-13 13:32:53 -0400

    Update to build RDPPacket from bytearray

--- packet.py
+++ packet.py
@@ -2,6 +2,9 @@ import struct
 
 from const import RDP_VERSION, PACKET_HEADER_STRUCT
 
+packet_header = struct.Struct(PACKET_HEADER_STRUCT)
+RDP_HEADER_SIZE = packet_header.size
+
 class RDPPacket:
     def __init__(
         self, data=None, version=RDP_VERSION, flags=0x0, opt_ptr=0x0, protocol=0x0, 
@@ -22,15 +25,14 @@ class RDPPacket:
         return 0x0
     
     def build_header(self):
-        packet_header = struct.Struct(PACKET_HEADER_STRUCT)
-
-        self.header_len = packet_header.size
+        self.header_len = RDP_HEADER_SIZE
         self.checksum = self.calculate_checksum()
         self.payload_len = 0
         if self.data:
             self.payload_len = len(self.data)
 
-        return packet_header.pack(
+        header = struct.Struct(PACKET_HEADER_STRUCT)
+        return header.pack(
             self.version,
             self.flags,
             self.opt_ptr,
@@ -44,6 +46,27 @@ class RDPPacket:
             self.window,
             self.payload_len
         )
+
+    def from_bytes(self, bytes):
+        payload = bytes[RDP_HEADER_SIZE:]
+        self.data = payload
+
+        header_data = bytes[:RDP_HEADER_SIZE]
+        header = struct.Struct(PACKET_HEADER_STRUCT)
+
+        fields = header.unpack(header_data)
+        self.version        = fields[0]
+        self.flags          = fields[1]
+        self.opt_ptr        = fields[2]
+        self.protocol       = fields[3]
+        self.header_len     = fields[4]
+        self.checksum       = fields[5]
+        self.source_port    = fields[6]
+        self.dest_port      = fields[7]
+        self.sequence_no    = fields[8]
+        self.ack_no         = fields[9]
+        self.window         = fields[10]
+        self.payload_len    = fields[11]
 
     def to_bytes(self):
         packet = self.build_header()
